5a1fae52554946b9247c56f2165b4d2c3ce64b55
Kacper Wardynski <kacper.wardynski@gmail.com>
2023-01-20 17:45:38 -0700

    feat(OA-10): Averaging function (#2)
    * add Decimal library to deps
    
    * aggregator function to extract OHLC values into a 1D array
    
    * started mathematics module with a mean function

--- lib/ohlc_analyzer/ohlc/record.ex
+++ lib/ohlc_analyzer/ohlc/record.ex
@@ -18,4 +18,16 @@ defmodule OhlcAnalyzer.Ohlc.Record do
     |> cast(attrs, [:timestamp, :open, :high, :low, :close])
     |> validate_required([:timestamp, :open, :high, :low, :close])
   end
+
+  @doc """
+  Combines the open, high, low, and close values from a singular Record or a
+  list of Records into a 1D array
+  """
+  def aggregate_record_values(records) when is_list(records) do
+    Enum.flat_map(records, &aggregate_record_values/1)
+  end
+
+  def aggregate_record_values(%__MODULE__{} = record) do
+    [record.open, record.high, record.low, record.close]
+  end
 end
